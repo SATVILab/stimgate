6d47c2308369737c88309d1744cd325516e18996
Miguel J Rodo <miguel.rodo@uct.ac.za>
2025-03-13 13:41:46 +0000

    Add plotting function

--- R/gates.R
+++ R/gates.R
@@ -1,36 +1,23 @@
-#' @param .data GatingSet.
-#' Original GatingSet algorithm was applied to, with the same name.
-#' @param pop_gate character.
-#' Name of population to gate.
-#' @param cut character vector.
-#' Channels to gate on.
-#' @param ind_in_batch_lab_vec character.
-#' I-th element specifies name of i-th sample within a batch.
-#' @param gate_name character. Name of gate.
-#' @param ind_in_batch_gate numeric vector.
-#' Specifies indices for each batch to gate.
-#' @param ind_in_batch_uns numeric.
-#' Specifies which index is unstim within each batch.
-#'
+#' @title Get gates
+#' 
+#' @description
+#' Get all the gates for each of the markers gated.
+#' 
+#' @param path_project character. Path to the project directory.
+#' 
 #' @return
 #' Gate table with gates for each sample for each marker.
 #' @export
-get_gate_tbl <- function(.data,
-                         pop_gate,
-                         chnl_cut,
-                         ind_in_batch_lab_vec,
-                         gate_name,
-                         ind_in_batch_gate,
-                         ind_in_batch_uns,
-                         path_project) {
-
-  purrr::map_df(chnl_cut, function(cut_curr) {
+get_gate_tbl <- function(path_project) {
+  dir_vec <- list.dirs(path_project, recursive = FALSE) |>
+    basename()
+  purrr::map_df(dir_vec, function(dir_curr) {
     # get stats tbl
-    gate_stats <- readRDS(file.path(path_project, cut_curr, "gate_stats_tbl"))
+    gate_tbl <- readRDS(file.path(path_project, dir_curr, "gate_tbl.rds"))
 
     gate_stats <- gate_stats |>
-      dplyr::mutate(chnl_cut = cut_curr, marker = chnl_lab_vec[cut_curr]) |>
-      dplyr::select(chnl_cut, marker, everything())
+      dplyr::mutate(chnl_cut = dir_curr) |>
+      dplyr::select(chnl_cut, everything())
 
     gate_stats
   })
