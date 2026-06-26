941d15abb592b738d1d28ac68a31c68d10899a9b
Miguel J Rodo <miguel.rodo@uct.ac.za>
2026-06-26 12:55:59 +0200

    Add back updates

---
+++ scripts/projr_build_dev.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=10
+#SBATCH --job-name="projr_build_dev"
+#SBATCH --partition=ada
+
+# Record the start time
+start_time=$(date +%s)
+
+echo "HOSTNAME: $HOSTNAME"
+
+echo " "
+echo " "
+echo " "
+
+echo "-------------------"
+echo "Run projr"
+date
+apptainer-rscript -f stimgate -- 'projr::projr_build_dev()'
+echo "Completed running projr"
+date
+echo "-------------------"
+echo " "
+
+# Record the end time
+end_time=$(date +%s)
+
+# Calculate the duration
+duration=$((end_time - start_time))
+
+# Convert duration to human-readable format
+hours=$((duration / 3600))
+minutes=$(( (duration % 3600) / 60 ))
+seconds=$((duration % 60))
+
+# Append the duration to the Slurm standard output log
+echo "--- Script Duration ---"
+printf "Elapsed time: %02d:%02d:%02d\n" $hours $minutes $seconds
+echo "-----------------------"
