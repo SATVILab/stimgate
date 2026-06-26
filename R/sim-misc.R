df64b9204a059de23cb33fe78d960d873900897d
Miguel J Rodo <miguel.rodo@uct.ac.za>
2026-06-26 12:55:59 +0200

    Add back updates

--- R/sim-misc.R
+++ R/sim-misc.R
@@ -7,3 +7,21 @@
     stop("transformation not recognised")
   )
 }
+
+calc_gamma <- function(x) {
+  gamma(1 + abs(x / 4))
+}
+calc_gaussian <- function(x) {
+  x
+}
+calc_skew <- function(x, epsilon = 2, delta = 1) {
+  cluster_mean <- mean(x)
+  cluster_sd <- sd(x)
+  weight <- 1 / (1 + exp(1.5 * (cluster_mean - 3.5)))
+  epsilon <- epsilon * weight
+  out <- sinh(epsilon + delta * asinh(x))
+  gamma_divisor <- rgamma(length(x), shape = 5, rate = 5)
+  out_fat <- out / sqrt(gamma_divisor)
+  (out - mean(out)) / sd(out) * sd(x) + cluster_mean
+}
+
