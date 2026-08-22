7505331c73ea0583b9aabccc1a47b042cdd142b3
Miguel J. Rodo <miguel.rodo@uct.ac.za>
2026-08-22 11:48:45 +0000

    Add script to run sheet 8 on SLURM

---
+++ scripts/slurm/dev-8-sim-compare-freq_bs-batch.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=5
+#SBATCH --job-name="dev-8-compare-batch"
+#SBATCH --partition=ada
+
+# Record the start time
+start_time=$(date +%s)
+
+echo "HOSTNAME: $HOSTNAME"
+
+echo " "
+echo " "
+echo " "
+
+echo "-------------------"
+echo "Run projr"
+date
+
+apptainer-rscript -f stimgate -- \
+  'projr::projr_build_dev(profile = "", file = "analysis/8-sim-compare-freq_bs-batch.qmd")'
+
+echo "Completed running projr"
+date
+echo "-------------------"
+echo " "
+
+# Record the end time
+end_time=$(date +%s)
+
+# Calculate the duration
+duration=$((end_time - start_time))
+
+# Convert the duration to human-readable format
+hours=$((duration / 3600))
+minutes=$(((duration % 3600) / 60))
+seconds=$((duration % 60))
+
+echo "--- Script Duration ---"
+printf "Elapsed time: %02d:%02d:%02d\n" \
+  "$hours" "$minutes" "$seconds"
+echo "-----------------------"
